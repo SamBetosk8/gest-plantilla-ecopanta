3ef53c0bf454f187566565d98a3139c44361e66a
Alan <alan.livelove51@gmail.com>
2026-04-09 12:14:25 -0400

    v0.1.3.2

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getDatabase } from 'firebase/database';
+import { getDatabase } from 'firebase/database'; 
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
@@ -14,5 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const rtdb = getDatabase(app);
+export const rtdb = getDatabase(app); 
 export const auth = getAuth(app);
